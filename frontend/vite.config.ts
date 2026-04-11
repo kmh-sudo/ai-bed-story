9184c3c9b3c528821311d7526a577ac439ee17bc
mtdev <kyawminhtet.1dev@gmail.com>
2026-04-11 23:20:50 +0700

    fix vite allow host error in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['.onrender.com', 'localhost']
+    allowedHosts: ['.onrender.com', 'localhost', 'storyai.duckdns.org']
   }
 })
